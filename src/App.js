e812686aafabaaf123dafa5a8a359859f6824bc1
Steven Dismuke <stevelp99@yahoo.com>
2019-11-13 09:46:29 -0600

    react connected to express

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Users from "./components/Users";
+import Posts from "./components/Posts";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Users />
+        <Posts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
